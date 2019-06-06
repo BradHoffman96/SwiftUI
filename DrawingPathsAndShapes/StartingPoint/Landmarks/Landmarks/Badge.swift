8ca77872debc25f2913831287e5f2728fe41bd3a
Brad Hoffman <bradleydavidhoffman@gmail.com>
2019-06-05 20:08:17 -0400

    started Drawing Paths and Shapes, but I have no current use for that and it was tedious, so I stopped. Also added the starting point for Animating Views and Transitions

---
+++ DrawingPathsAndShapes/StartingPoint/Landmarks/Landmarks/Badge.swift
@@ -0,0 +1,53 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Bradley Hoffman on 6/5/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge : View {
+    var body: some View {
+        Path { path in
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(
+                to: CGPoint(
+                    x: width * 0.95,
+                    y: height * (0.20 + HexagonParameters.adjustment)
+                )
+            )
+            
+            HexagonParameters.points.forEach({
+                path.addLine(
+                    to: .init(
+                        x: width * $0.useWidth.0 * $0.xFactors.0,
+                        y: height * $0.useHeight.0 * $0.yFactors.0
+                    )
+                )
+                
+                path.addQuadCurve(
+                    to: .init(
+                        x: width * $0.useWidth.1 * $0.xFactors.1,
+                        y: height * $0.useHeight.1 * $0.yFactors.1
+                    ),
+                    control: .init(
+                        x: width * $0.useWidth.2 * $
+                    )
+                    
+                )
+            })
+        }
+        .fill(Color.black)
+    }
+}
+
+#if DEBUG
+struct Badge_Previews : PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
+#endif
